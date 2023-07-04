833ad419086bfce6019890a8696ececfeaf0cb0d
Dennis O'Keeffe <hello@dennisokeeffe.com>
2023-07-04 11:06:53 +1000

    feat(NOD-374): implement functionality for separateIntoMatchInput

--- src/util/separateIntoMatchInput.ts
+++ src/util/separateIntoMatchInput.ts
@@ -2,5 +2,15 @@
  * Helper that takes rawInput string that was parsed from a file and returns an array of strings.
  */
 export function separateIntoMatchInput(rawInput: string): string[] {
-  return []
+  return (
+    rawInput
+      // Split on match
+      .split(/Match:/)
+      // Remove empty strings
+      .filter((match) => match !== '')
+      // Remove whitespace
+      .map((str) => str.trim())
+      // Add match back to beginning of string
+      .map((str) => `Match: ${str}`)
+  )
 }
