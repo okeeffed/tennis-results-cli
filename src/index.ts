96cf9a7e5a4c0d6e5ddf80c55a4a5b5c8b53709d
Dennis O'Keeffe <hello@dennisokeeffe.com>
2023-07-04 20:44:26 +1000

    feat(NOD-385): support for stdin messages on CLI + argument

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+import yargsParser from 'yargs-parser'
+const argv = yargsParser(process.argv.slice(2))
+
+function delegateToCommand(input: string) {
+  if (/Score Match/.test(input)) {
+    const matchId = input.split('Score Match ')[1]
+    console.log(matchId)
+  } else if (/Games Player/.test(input)) {
+    const player = input.split('Games Player ')[1]
+    console.log(player)
+  } else {
+    throw new Error(`Unexpected input: ${input}`)
+  }
+}
+
 function main() {
-  console.log('Hello World!')
+  const [inputFile] = argv._
+
+  if (!inputFile) {
+    throw new Error('Missing input file.')
+  }
+
+  // Handle commands from stdin and take action from there.
+  process.stdin.on('data', (data) => {
+    data.toString().trim().split('\n').map(delegateToCommand)
+  })
 }
 
 main()
